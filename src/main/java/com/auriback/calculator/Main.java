6801027d0bf8eb1415e855d288518c0c24f329dc
auriback <muslimovatanya@gmail.com>
2026-09-21 00:27:07 +0500

    ref: method calculate throws exception if division by zero

--- src/main/java/com/auriback/calculator/Main.java
+++ src/main/java/com/auriback/calculator/Main.java
@@ -20,17 +20,13 @@ public class Main {
                 String operator = vals[1];
                 int b = Integer.parseInt(vals[2]);
 
-                if (operator.equals("/") && b == 0) {
-                    System.out.println("Ошибка: деление на ноль");
-                }
-
                 int result = Calculator.calculate(a, b, operator);
                 System.out.println(a + " " + operator + " " + b + " = " + result);
             } catch (NumberFormatException e) {
                 System.out.println("Ошибка: неверный формат. Используйте: число оператор число");
+            } catch (ArithmeticException e) {
+                System.out.println("Ошибка: деление на ноль");
             }
         }
     }
-
-
 }
--- src/main/java/com/auriback/calculator/calculator/Calculator.java
+++ src/main/java/com/auriback/calculator/calculator/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
     private Calculator() {}
 
     public static int calculate(int a, int b, String operator) {
+        if (operator.equals("/") && b == 0) {
+            throw new ArithmeticException("Ошибка: деление на ноль");
+        }
         return switch (operator) {
             case "+" -> a + b;
             case "-" -> a - b;
